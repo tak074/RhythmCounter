dfd183c0014278ec9e5acdbebf362c842bc18beb
root <tak074@ucsd.edu>
2020-11-24 19:42:53 -0800

    created package.json, webpack, and basic react app page through react and express

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, 'client', 'src');
+var DIST_DIR = path.join(__dirname, 'client', 'dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  mode: "development",
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+  },
+  watch: true,
+  module : {
+    rules : [
+      {
+        test : /\.jsx?/,
+        include : SRC_DIR,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env', 'babel-preset-react']
+          }
+        }
+      },
+      {
+        test: /\.(jpg|png)$/,
+        include : SRC_DIR,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'url-loader',
+        },
+      }
+    ]
+  }
+};
